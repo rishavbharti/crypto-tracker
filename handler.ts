714144911fca83ffc10e63fae86850f93665d727
Rishav Bharti <rishavbharti21@gmail.com>
2021-09-26 21:55:26 +0530

    CPT12: Create portfolio service to fetch users crypto portfolio

--- handler.ts
+++ handler.ts
@@ -1,16 +1,59 @@
-import * as aws from 'aws-sdk'
+import * as aws from "aws-sdk";
 
-aws.config.loadFromPath('./config.json')
+aws.config.loadFromPath("./config.json");
 
-export const handle = async (event: any, context: any) => {
+var docClient = new aws.DynamoDB.DocumentClient();
+
+const TABLE_NAME = "SkrBootstrap-CryptoPortfolioTracker-rishav-UserTable";
+
+function response(statusCode: number, message: any) {
     return {
-        statusCode: 200,
+        statusCode: statusCode,
         headers: {
             "Content-Type": "*/*",
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "*"
+            "Access-Control-Allow-Methods": "*",
         },
-        body: "Hello!"
-    }
-};
+        body: JSON.stringify(message),
+    };
+}
 
+export const handle = async (event: any, context: any) => {
+    const username = event.queryStringParameters?.username;
+
+    if (!username) {
+        return response(400, {
+            error: "Username must be present in the query params",
+        });
+    }
+
+    const params = {
+        TableName: TABLE_NAME,
+        KeyConditionExpression: "username = :username",
+        ExpressionAttributeValues: {
+            ":username": username,
+        },
+    };
+
+    return docClient
+        .query(params)
+        .promise()
+        .then((res) => {
+            const { Count, Items } = res;
+            let responseData;
+
+            if (Count === 0 || !Items[0].assets) {
+                responseData = [];
+            } else {
+                responseData = Object.entries(Items[0].assets).map((asset) => {
+                    return { token: asset[0], ...asset[1] };
+                });
+            }
+
+            return response(200, responseData);
+        })
+        .catch((err) => {
+            console.log(err);
+            return response(err.statusCode, err);
+        });
+};
